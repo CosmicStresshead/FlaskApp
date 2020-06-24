d70b1f6b5de72625a3679c4fa53dce3d9026d132
CosmicStresshead <bensmylie@gmail.com>
2020-06-24 15:57:17 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,25 @@
+def article_data():
+    data = [
+            {  
+              'id': 1,
+              'title': "One",
+              'body': "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+              'author': "Ben Smylie",
+              'create_date': "22-06-2020"
+            },
+            {
+              'id': 2,
+              'title': "Two",
+              'body': "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+              'author': "Jane Doe",
+              'create_date': "23-06-2020"
+            },
+            {
+              'id': 3,
+              'title': "Three",
+              'body': "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+              'author': "Ben Smylie",
+              'create_date': "24-06-2020"
+            }
+           ]
+    return data
